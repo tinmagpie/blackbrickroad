f20867160cd8e72da09c0afe6b8902c1f3c62213
Rachel Nabors <rachelnabors@gmail.com>
2014-07-07 20:08:39 -0700

    Setting up the sassery. It's sass-tastic.

--- config.rb
+++ config.rb
@@ -1,4 +1,6 @@
 # Require any additional compass plugins here.
+require 'singularitygs'
+require 'breakpoint'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -8,13 +10,13 @@ images_dir = "img"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-relative_assets = true
+# relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
